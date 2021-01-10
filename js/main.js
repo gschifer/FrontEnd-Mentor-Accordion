d8bcb3814816f3fea142cb7d2cd9061ada5c7543
gschifer <gabriel.schifer@hotmail.com>
2021-01-10 14:12:52 -0300

    a tiny change to make a best screenshot at frontendmentor

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ var arrow = document.getElementsByClassName("arrow");
 var item  = document.getElementsByClassName("item");
 var panel = document.getElementsByClassName("panel");
 
+item[1].classList.add("active");
+arrow[1].style.transform = "rotate(180deg)";
+panel[1].style.display   = "block";
+
 for (let i = 0; i < arrow.length; i++) {
     item[i].onclick = () => {
         
